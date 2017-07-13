33f5e29a1e52ca64bb6ca8d377c289339613295b
umadevikvus <uma1413113@gmail.com>
2017-07-13 12:27:55 +0530

    Add files via upload

---
+++ AssignNum.java
@@ -0,0 +1,16 @@
+package guvi1;
+
+import java.util.Scanner;
+
+public class AssignNum {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int num=in.nextInt();
+		in.close();
+		System.out.println((char)(num+64));
+		
+
+	}
+
+}

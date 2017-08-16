dcab3e904f1fe8d3c66c78b88aa36eb1d6fc7a21
umadevikvus <uma1413113@gmail.com>
2017-08-16 21:24:28 +0530

    Add files via upload

--- Pattern.java
+++ Pattern.java
@@ -1,4 +1,4 @@
-package guvi1;
+package guvi;
 
 import java.util.Scanner;
 
@@ -8,11 +8,17 @@ public class Pattern {
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
 		in.close();
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				System.out.print("*");
+		int i=0;
+		int j=0;
+		int k=0;
+		for(i=0;i<=n;i++){
+			for(j=1;j<=i;j++){
+				System.out.print(j);
 			}
-			System.out.println();
+			for(k=j-2;k>=1;k--){
+				System.out.print(k);
+			}
+			System.out.print(" ");
 		}
 
 	}

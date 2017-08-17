0259debc4c0cc858b33dacdd979f5d223c4cd065
umadevikvus <uma1413113@gmail.com>
2017-08-17 21:49:14 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,25 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int num=in.nextInt();
+		in.close();
+		int n1=0;
+		int n2=1;
+		int n3=0;
+		System.out.print(n1+" "+n2);
+		for(int i=0;i<num;i++) {
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+				
+
+	}
+
+}
